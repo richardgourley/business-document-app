e0db9dda2918f6e3aa780faa6097bb0d844912eb
Rich <codeleap@outlook.com>
2021-01-19 20:56:36 +0100

    Main menu file added - display_options method added

---
+++ mainmenu.py
@@ -0,0 +1,11 @@
+class MainMenu:
+    def __init__(self):
+        self.display_options()
+
+    def display_options(self):
+        print('Hello, what is your name?')
+        name = input()
+        print('Hello ' + name + '!!!') 
+        print('Choose an option from the menu below:')
+
+        
