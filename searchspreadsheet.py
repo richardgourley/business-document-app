919926f55eb6d3bdbc69fa28197e79a1aae0337c
Rich <codeleap@outlook.com>
2021-01-21 13:09:00 +0100

    Get column names added. methods inside __init__ added

--- searchspreadsheet.py
+++ searchspreadsheet.py
@@ -9,6 +9,54 @@ class SearchSpreadsheet:
         self.choose_search_term()
         self.print_search_results()
 
+    def check_excel_files_exist(self):
+        os.chdir('excelfiles')
+        self.excel_files = os.listdir()
+        if len(self.excel_files) == 0:
+            print('============')
+            print("We couldn't find any files in the 'excelfiles' directory. Please add some files and run the program again.")
+            print('============')
+            quit()
+
+    def display_available_files(self):
+        print('Here are the available excel files you can search.')
+        print('===============')
+        for file in self.excel_files:
+            print(file)
+        print('===============')
+
+    def choose_excel_file(self):
+        excel_file = None
+        while excel_file is None:
+            # User chooses file
+            print("Enter the name of a file to search. (Must match a file name shown above)")
+            excel_file = input()
+            try:
+                excel_file = openpyxl.load_workbook(excel_file)
+            except:
+                excel_file = None
+                print("Sorry, the file name you entered doesn't match an available file.")
+        
+        self.excel_file = excel_file
+        self.sheet = self.excel_file.active
+        print("===============")
+
+    def get_column_names(self):
+        # get column names as a string for printing
+        self.column_names = ""
+        for i in range(1, self.sheet.max_column + 1):
+            self.column_names += str(self.sheet[get_column_letter(i) + "1"].value) + " -- "
+
+    def choose_search_term(self):
+        search_term = ""
+        # If 'search_term' is blank, we repeat the loop
+        while search_term == "":
+            print("Enter a search term. You can enter words or numbers. (Can't be blank)")
+            search_term = input()
+        self.search_term = search_term
+
+        print("=============")
+
     # Returns a data type of results and result information
     def print_search_results(self):
         # count will display how many results we find
